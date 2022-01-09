c1a437e9d5a5bc2cf68de5ff812f17bc2e5709b6
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2022-01-09 17:56:25 +0530

    more_code...

--- week1/classwork/blocked-billboard.cpp
+++ week1/classwork/blocked-billboard.cpp
@@ -1,13 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {  
-    bool cover (2001);
-    int b1x1, b1y1, b1x2, b1y2;
-    int b2x1, b2y1, b2x2, b2y2;
-    int trx1, try1, trx2, try2;
+struct Rect {
+    int x1, y1, x2, y2;
+    int area() {
+        return (abs(x2 - x1) * abs(y2 - y1));
+    }
+};
 
-    cin >> b1x1 >> b1x1 >> b1x2 >> b1y2;
-    cin >> b2x1 >> b2y1 >> b2x2 >> b2y2;
-    cin >> trx1 >> try1 >> trx2 >> try2;
+int intersection(struct Rect r1, struct Rect r2) {
+    int x_overlap = max(0, min(r1.x1 + r1.x2, r2.x1 + r2.x2) - max(r1.x1, r2.x1));
+    int y_overlap = max(0, min(r1.y1 + r1.y2, r2.y1 + r2.y2) - max(r1.y1, r2.y1));
+    int overlapArea = x_overlap * y_overlap;
+    return overlapArea;
+}
+
+int main() {
+    struct Rect billboard1, billboard2, truck;
+    cin >> billboard1.x1 >> billboard1.y1 >> billboard1.x2 >> billboard1.y2;
+    cin >> billboard2.x1 >> billboard2.y1 >> billboard2.x2 >> billboard2.y2;
+    cin >> truck.x1 >> truck.y1 >> truck.x2 >> truck.y2;
+
+    int billboard1_area = billboard1.area();
+    int billboard2_area = billboard2.area();
+    int intersection_tr_b1 = intersection(billboard1, truck);
+    int intersection_tr_b2 = intersection(billboard2, truck);
+    int area = billboard1_area + billboard2_area - (intersection_tr_b1 + intersection_tr_b2);
+    cout << area;
+
 }
