73c21e6eaafbd1987407278f23c5d0430ae22db1
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2022-01-25 21:24:04 +0530

    triangles... whyyyy

---
+++ week3/homework/p2-triangles.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int max_n = 100;
+
+int main() {
+    int N;
+    vector<pair<int, int>> coordinates (max_n);
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> coordinates[i].first >> coordinates[i].second;
+    }
+
+    
+}
