3eaa1fcb1c479e6773c1f89c317dae39bcf346e3
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2022-01-26 22:53:40 +0530

    triangles

--- week3/homework/p2-triangles.cpp
+++ week3/homework/p2-triangles.cpp
@@ -1,45 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define MAX_N 100
-int N = 0;
-vector<pair<int, int>> coordinates (MAX_N);
-
-void input(int N) {
-    for (int i = 0; i < N; i++) {
-        cin >> coordinates[i].first >> coordinates[i].second;
-    }
-}
-
-void file_input(string c) {
-    freopen(c + ".in", "r", stdin);
-    freopen(c + ".out", "w", stdout);
-}
-
-void fast_io() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-}
-
-void parallel(float a1, float b1,
-              float c1, float a2,
-              float b2, float c2)
-{
-    // If slopes are equal
-    if ((-(a1 / b1)) == (-(a2 / b2))) {
-        cout << "Yes";
-    }
-    else {
-        cout << "No";
-    }
-}
-
-void solve() {
-    cin >> N;
-    input(N);
-    
-}
+const int MAX_N = 105;
 
 int main() {
-    fast_io();
+	int n; cin >> n;
+	int x[MAX_N];
+	int y[MAX_N];
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> x[i] >> y[i];
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				// Check to see if both points are parallel to the X and Y axis
+				if (y[i] == y[j] && x[i] == x[k]) {
+					int area = abs((x[j] - x[i]) * (y[k] - y[i]));
+					ans = max(ans, area);
+				}
+			}
+		}
+	}
+	cout << ans << endl;
 }
