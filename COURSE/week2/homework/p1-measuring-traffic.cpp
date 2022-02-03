26c0d1ae0546fa0878398b952a2fcc671b8e8a5f
Sha2020-ship-it <vishal.study@gmail.com>
2022-02-03 11:57:59 +0530

    more

--- COURSE/week2/homework/p1-measuring-traffic.cpp
+++ COURSE/week2/homework/p1-measuring-traffic.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+    int n;
+    int days = 0;
+    string cow = 0;
+    signed int diff;
 
+    cin >> n;
+    vector<tuple<int, string, int>> logs;
+
+    for (int i = 0; i < n; i++) {
+        cin >> days >> cow >> diff;
+        logs.push_back(make_tuple(days, cow, diff));
+    }
+
+    sort(logs.begin(), logs.end());
+    for (int i = 0; i < logs.size(); i++) {
+        cout << 
+    }
 }
