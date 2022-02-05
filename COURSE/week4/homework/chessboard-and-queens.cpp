19353545454085ce7ee702b77665181758f1440e
Sha2020-ship-it <vishal.study@gmail.com>
2022-02-05 16:02:30 +0530

    more

--- COURSE/week4/homework/chessboard-and-queens.cpp
+++ COURSE/week4/homework/chessboard-and-queens.cpp
@@ -1,6 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
-
+    vector<string> board (8);
+    for (int i = 0; i < 8; i++) {
+        cin >> board[i];
+    }
 }
